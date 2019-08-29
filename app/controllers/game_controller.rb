ca31c1c47d0f91400c3b8fcf00331c23d948e2b7
Francis Robichaud <skaber@gmail.com>
2019-08-29 11:59:05 +0200

    Make the game functional

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,15 +1,24 @@
 class GameController < ApplicationController
     def selection
       payload = JSON.parse params[:payload]
-      puts payload
-      puts payload
-      puts payload
+      
       if payload['type'] == 'interactive_message'
+        attachment = payload['original_message']['attachments'].first
+        answer = attachment['actions'].find {|action| action['value'] == '1'}
+        
         #TODO verify success/failure
-        #if payload[:actions].any?()
-        puts 'zzzzz'
-        #FaceMatch::SlackHelper.reply_success(event[:channel], 'TODO')
+        if payload['actions'].first['value'] == '1'  
+          result = {
+            replace_original: false,
+            text: '✅ Good job! That was ' + answer['text']
+          }
+        else
+          result = {
+            replace_original: false,
+            text: '❌ Nope! That was ' + answer['text']
+          }
+        end
       end
-      json: {replace_original: false}
+      render json: result
     end
 end
--- app/helpers/face_match/slack_helper.rb
+++ app/helpers/face_match/slack_helper.rb
@@ -5,44 +5,46 @@ module FaceMatch
     @@DEBUG = true
 
     def self.fetch_profiles(current_user)
-      profile = random_profile(current_user)
-      {
-        name: profile.real_name,
-        picture: profile.image_512
-      }
+      profiles = generate_profiles(current_user)
+      results = profiles.map.with_index do |profile, index|
+        {
+          id: profile.id,
+          name: profile.profile.real_name,
+          picture: profile.profile.image_192,
+          answer: index == 0
+        }
+      end
+      results.shuffle
     end
 
-    def self.send_quiz_message(selected_profile, channel)
+    def self.send_quiz_message(profiles, channel)
       @client.chat_postMessage(channel: channel,
                                         as_user: true,
                                         text: 'WHO DIS?',
                                         attachments: [{
-                                            type: 'image',
-                                            title: {},
-                                            image_url: selected_profile[:picture],
-                                            attachment_type: "default",
-                                            callback_id: 'name',
-                                            actions: [{
-                                              name: 'first',
-                                              text: selected_profile[:name],
+                                          type: 'image',
+                                          title: {},
+                                          image_url: profiles.first[:picture],
+                                          attachment_type: "default",
+                                          callback_id: 'name',
+                                          actions: profiles.map do |profile| 
+                                            {
+                                              name: profile[:id],
+                                              text: profile[:name],
                                               type: 'button',
-                                              value: '0'
-                                            }]
-                                          }
+                                              value: profile[:answer]
+                                            }
+                                          end
+                                        }
                                         ])
     end
 
     def self.help(channel)
       @client.chat_postMessage(channel: channel,
                                         as_user: true,
-                                        text: 'Type "game" to receive the next profile picture challenge, and "stop" if you no longer want to receive messages.')
+                                        text: 'Type "play" to receive a random profile picture challenge. "sync" if you need to force updating the organization user list.')
     end
 
-    def self.reply_success(answer, channel)
-      @client.chat_postMessage(channel: channel,
-                                        as_user: true,
-                                        text: '✅Good job!✅, that was ' + answer)
-    end
 
     def self.reply_failure(answer, channel)
       @client.chat_postMessage(channel: channel,
@@ -51,20 +53,21 @@ module FaceMatch
     end
 
     private
-    def self.random_profile(current_user)
+    def self.generate_profiles(current_user)
       members = @client.users_list[:members]
-      random_pick = rand(0...members.count)
-      profile = members[random_pick]
-      puts '#####'
-      puts profile.is_bot
-      puts profile[:id]
-      puts current_user
-      puts '#####'
+      first_rnd = members[rand(0...members.count)]
+      second_rnd = members[rand(0...members.count)]
+      selection = pick_selection([current_user, first_rnd.id, second_rnd.id], members)      
+      [selection, first_rnd, second_rnd]
+    end
+
+    def self.pick_selection(user_filter, members)
+      member = members[rand(0...members.count)]
       # Skip bots, avoid the current_user and make sure there's a picture
-      if (profile.is_bot || profile[:id] == current_user || profile[:id] == 'USLACKBOT' || profile[:picture].nil?) && !@@DEBUG
-        profile = random_profile(current_user) #TODO: if no user have profile picture, we'll end up with an infinite loop until Slack::Web::Api::Errors::TooManyRequestsError
+      if (member.is_bot || user_filter.include?(member[:id]) || member[:id] == 'USLACKBOT' || member.profile[:image_192].nil?) && !@@DEBUG
+        profile = pick_selection(user_filter, members) #TODO: if no user have profile picture, we'll end up with an infinite loop until Slack::Web::Api::Errors::TooManyRequestsError
       end
-      profile
+      member
     end
   end
 end
